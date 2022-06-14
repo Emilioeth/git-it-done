4f601eb47f994d4a7c2afd312a60061067c072f4
Emilio <emiliothomasking@gmail.com>
2022-06-14 13:08:29 -0700

    added function to dynamically fecth  user repos

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,13 @@
-var getUserRepos = function() {
-    fetch("https://api.github.com/users/octocat/repos");
-  };
+var getUserRepos = function(user) {
+    // format the github api url
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
   
-  getUserRepos();
+    // make a request to the url
+    fetch(apiUrl).then(function(response) {
+      response.json().then(function(data) {
+        console.log(data);
+      });
+    });
+  };  
+
+getUserRepos();
